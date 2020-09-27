9219b44a9ad8b821a7c1168c9e31369095f1c210
Filipe Câncio <filipe.cancio@outlook.com>
2020-09-27 18:24:49 -0300

    feat: update site content #2

--- application/src/App.js
+++ application/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const repo = {
+  title:"Eugenio Challenge Hub",
+  hackathin_logo_url: "https://www.eugeniochallengehub.io/wp-content/themes/challengehub/img/logo.png",
+  eugenio_logo_url: "https://www.eugeniochallengehub.io/wp-content/themes/challengehub/img/eugenio.gif",
+  repo_url:"https://github.com/filipecancio/eugenio-challenge"
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={repo.eugenio_logo_url} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A aplicação está em desenvolvimento.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={repo.repo_url}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          repositório
         </a>
       </header>
     </div>
