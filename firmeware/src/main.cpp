1047791bb02be3a99728f99bda92fd3fd017d09c
Filipe Câncio <filipe.cancio@outlook.com>
2020-10-04 13:02:42 -0300

    feat: connect the board to wifi #5

--- firmeware/src/main.cpp
+++ firmeware/src/main.cpp
@@ -1,14 +1,8 @@
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-
-#include "Arduino.h"
+#include <Arduino.h>
+#include <secrets.h>
 
 void setup()
 {
-  // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
@@ -17,9 +11,9 @@ void loop()
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
-  delay(1000);
+  delay(500);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
    // wait for a second
-  delay(1000);
+  delay(500);
 }
